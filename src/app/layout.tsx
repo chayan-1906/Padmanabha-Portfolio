f5a1ce669ec66c64e1f6a0376eeb8bbda951c531
chayan-1906 <chayan19062000@gmail.com>
2025-07-13 21:29:36 +0530

    working theme system with CSS variables and visible toggle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,7 @@ export default function RootLayout({
 			defaultTheme="system"
 			enableSystem
 			disableTransitionOnChange={false}
+			forcedTheme={undefined}
 		>
 			{children}
 		</ThemeProvider>
